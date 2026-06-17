d377db410da52f9614f3bf3757c242744a0c16c3
Aobakwe-Bogatsu <aobakwe.bogatsu59@gmail.com>
2026-06-17 19:49:13 +0200

    fix: clean Vercel deployment build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/aobakwe-portfolio/',
-
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
